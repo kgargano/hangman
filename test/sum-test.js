d86870f361ff214b7bbdd539b640d4e95642fca1
Keith Gargano <keith.gargano@versionone.com>
2015-05-28 12:10:03 -0400

    sum function

---
+++ test/sum-test.js
@@ -0,0 +1,6 @@
+var assert = require("assert")
+describe('Sum', function(){
+        it('should return the sum when given two values', function(){
+            assert.equal(10, sum(5,5));
+        });
+});
