22f5c600fba747d67782a1da1cb5b745e8f52a78
mickeyrogers <mickeyrogers123@gmail.com>
2015-05-28 12:55:26 -0400

    Remove unnecessary test

--- test/sum-test.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-var Sum = require("../src/js/sum.js");
-
-console.log(Sum);
-describe('Sum', function(){
-        it('should return the sum when given two values', function(){
-            assert.equal(10, Sum.sum(5,5));
-        });
-});
